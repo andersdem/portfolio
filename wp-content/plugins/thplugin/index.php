e38477959c5c40dfc0e2d365607b6d889fe05ee2
andersdem <56107522+andersdem@users.noreply.github.com>
2020-10-20 11:20:23 +0200

    pluuuug

---
+++ wp-content/plugins/thplugin/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Partyvibes Plugin 
+* Plugin URI: http://http://localhost/git/portfolio/
+* Description: This is a Newsletter Signup form Plugin for Cheers based on HTML5, CSS, JS and PHP
+* Version: 2.9.9
+* Author: Murat Kilic
+* Author URI: http://http://localhost/git/portfolio/
+* License: GPL2
+*/
